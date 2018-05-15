285ad878ba0b72a4a49085b3094854633189254d
pioj <piojete@gmail.com>
2018-05-15 21:05:14 +0200

    todo listo para metodo async
    script base creado, ahora le pondré el async

---
+++ Assets/Scripts/AsyncLoad.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class AsyncLoad : MonoBehaviour {
+
+    public Text TextoBoton;
+    public Transform CuboRota;
+    public CanvasGroup mycanvasgroup;
+    public Transform progress;
+
+    // cuando pulso
+    public void Cargar() {
+        TextoBoton.text = "Cargando";
+        mycanvasgroup.interactable = false;
+
+        //el cubo y carga async
+    }
+}
